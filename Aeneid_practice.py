49baf46c72e402b1dbe966916870b0642b769084
Daniel Libatique <daniellibatique@Daniels-MacBook-Pro-4.local>
2017-08-21 21:17:33 -0400

    created file

---
+++ Aeneid_practice.py
@@ -0,0 +1,3 @@
+Aeneid_1 = 'arma virumque cano Troiaeque primus ab oris \n Italiam fato profugus Laviniaque venit \n litora multum ille et terris iactatus et alto'
+Aeneid_1.split('\n')
+print Aeneid_1
