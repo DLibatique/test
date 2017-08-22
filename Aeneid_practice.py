8a3001b2a37cc86166e587e190e4897511d03c47
Daniel Libatique <daniellibatique@Daniels-MacBook-Pro-4.local>
2017-08-21 21:28:11 -0400

    reversed the variable names and what they equal

--- Aeneid_practice.py
+++ Aeneid_practice.py
@@ -1,8 +1,8 @@
 raw_text = 'arma virumque cano Troiaeque primus ab oris\nItaliam fato profugus Laviniaque venit\nlitora multum ille et terris iactatus et alto'
 Aeneid_1 = raw_text.split('\n')
-Aeneid_1[0] = Aeneid_1_0001
-Aeneid_1[1]	= Aeneid_1_0002
-Aeneid_1[2] = Aeneid_1_0003
+Aeneid_1_0001 = Aeneid_1[0] 
+Aeneid_1_0002 = Aeneid_1[1] 
+Aeneid_1_0003 = Aeneid_1[2]
 
 print Aeneid_1_0001
 print Aeneid_1_0002
