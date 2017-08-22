8be0540a776984c41ff45b54834d6566fbd811a0
Daniel Libatique <daniellibatique@Daniels-MacBook-Pro-4.local>
2017-08-21 21:25:31 -0400

    started listing

--- Aeneid_practice.py
+++ Aeneid_practice.py
@@ -1,3 +1,9 @@
-Aeneid_1 = 'arma virumque cano Troiaeque primus ab oris \n Italiam fato profugus Laviniaque venit \n litora multum ille et terris iactatus et alto'
-Aeneid_1.split('\n')
-print Aeneid_1
+raw_text = 'arma virumque cano Troiaeque primus ab oris\nItaliam fato profugus Laviniaque venit\nlitora multum ille et terris iactatus et alto'
+Aeneid_1 = raw_text.split('\n')
+Aeneid_1[0] = Aeneid_1_0001
+Aeneid_1[1]	= Aeneid_1_0002
+Aeneid_1[2] = Aeneid_1_0003
+
+print Aeneid_1_0001
+print Aeneid_1_0002
+print Aeneid_1_0003
