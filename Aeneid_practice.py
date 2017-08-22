4ea9af8f27db61c0dbc009941faa79cffef03887
Daniel Libatique <daniellibatique@Daniels-MacBook-Pro-4.local>
2017-08-21 22:14:27 -0400

    added last print

--- Aeneid_practice.py
+++ Aeneid_practice.py
@@ -1,9 +1,10 @@
-raw_text = 'arma virumque cano Troiaeque primus ab oris\nItaliam fato profugus Laviniaque venit\nlitora multum ille et terris iactatus et alto'
-Aeneid_1 = raw_text.split('\n')
-Aeneid_1_0001 = Aeneid_1[0] 
-Aeneid_1_0002 = Aeneid_1[1] 
-Aeneid_1_0003 = Aeneid_1[2]
+raw_text = 'arma virumque cano Troiae qui primus ab oris\nItaliam fato profugus Laviniaque venit\nlitora multum ille et terris iactatus et alto'
+line_list = raw_text.split('\n')
 
-print Aeneid_1_0001
-print Aeneid_1_0002
-print Aeneid_1_0003
+Aeneid_1 = {}
+
+for line_number, text in enumerate(line_list):
+  Aeneid_1[line_number+1] = text.split()
+
+print Aeneid_1
+print Aeneid_1[1][2][-1]
